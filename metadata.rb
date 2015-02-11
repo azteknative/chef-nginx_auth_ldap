1d0552f66734b09e797f3064519945a44aaf7c12
Peter Green <peter.green@aztek-native.com>
2015-02-11 11:30:42 +0000

    Initial commit - blank cookbook.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'nginx-auth-ldap'
+maintainer       'AzTek Native Ltd.'
+maintainer_email 'peter.green@aztek-native.com'
+license          'All rights reserved'
+description      'Installs/Configures nginx-auth-ldap'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: nginx-auth-ldap
+# Recipe:: default
+#
+# Copyright 2015, AzTek Native Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
