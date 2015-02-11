3a25caa7184394c630f0b6018022d089340a31d3
Peter Green <peter.green@aztek-native.com>
2015-02-11 17:15:16 +0000

    Updated license.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'nginx_auth_ldap'
 maintainer       'AzTek Native Ltd.'
 maintainer_email 'peter.green@aztek-native.com'
-license          'All rights reserved'
+license          'Apache License, version 2.0'
 description      'Installs/Configures nginx_auth_ldap'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
