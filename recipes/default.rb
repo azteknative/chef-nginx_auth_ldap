90013bf365a300d673f124710df1502ba72033e6
Peter Green <peter.green@aztek-native.com>
2015-02-11 17:05:27 +0000

    Default recipe sets up nginx cookbook attributes and calls nginx::source

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,13 @@
 #
-# Cookbook Name:: nginx-auth-ldap
+# Cookbook Name:: nginx_auth_ldap
 # Recipe:: default
 #
 # Copyright 2015, AzTek Native Ltd.
 #
 # All rights reserved - Do Not Redistribute
 #
+
+node.default['nginx']['source']['modules'] << 'nginx_auth_ldap::ngx_ldap_module'
+
+include_recipe "nginx::source"
+
